d6685969111e5382d475c1926a8a3c191f7a61f5
Vitaliy Tsyganok <vitalytsiganok@gmail.com>
2022-12-03 15:27:53 +0400

    2.6 Взаимодействие alert, prompt, confirm

---
+++ ЧАСТЬ 1. Язык JavaScript/2.6 Взаимодействие alert, prompt, confirm/script.js
@@ -0,0 +1,9 @@
+/* https://learn.javascript.ru/task/simple-page
+
+Простая страница
+важность: 4
+Создайте страницу, которая спрашивает имя у пользователя и выводит его. */
+
+const userName = prompt('Как Вас зовут?', '')
+
+alert(`Вас зовут ${userName}`)
