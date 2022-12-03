e8eba18cd7a2b9880b95ab4288fbb05406d65310
Vitaliy Tsyganok <vitalytsiganok@gmail.com>
2022-12-03 19:29:14 +0400

    2.8 Базовые операторы, математика (+4 задачи)

---
+++ ЧАСТЬ 1. Язык JavaScript/2.8 Базовые операторы, математика/02-Результат присваивания/script.js
@@ -0,0 +1,12 @@
+/* https://learn.javascript.ru/task/assignment-result
+
+Результат присваивания
+важность: 3
+Чему будут равны переменные a и x после исполнения кода в примере ниже? */
+
+let a = 2
+
+let x = 1 + (a *= 2)
+
+console.log(a) // 4
+console.log(x) // 5
