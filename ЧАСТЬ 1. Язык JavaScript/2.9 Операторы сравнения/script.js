4c9f73defb6f56e117051a17c4c93f3e8bdd8daf
Vitaliy Tsyganok <vitalytsiganok@gmail.com>
2022-12-05 12:00:51 +0400

    2.9 Операторы сравнения

---
+++ ЧАСТЬ 1. Язык JavaScript/2.9 Операторы сравнения/script.js
@@ -0,0 +1,13 @@
+/* https://learn.javascript.ru/task/comparison-questions
+
+Операторы сравнения
+важность: 5
+Каким будет результат этих выражений? */
+
+5 > 4 // true
+"ананас" > "яблоко" // false
+"2" > "12" // true
+undefined == null // true
+undefined === null // false
+null == "\n0\n" // false
+null === +"\n0\n" // false
