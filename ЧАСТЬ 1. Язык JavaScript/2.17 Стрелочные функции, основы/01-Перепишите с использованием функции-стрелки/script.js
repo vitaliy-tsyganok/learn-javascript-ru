e2c8b456c94645131d09c7cea25257dc137aad23
Vitaliy Tsyganok <vitalytsiganok@gmail.com>
2022-12-10 09:49:46 +0400

    2.17 Стрелочные функции, основы (1 задача)

---
+++ ЧАСТЬ 1. Язык JavaScript/2.17 Стрелочные функции, основы/01-Перепишите с использованием функции-стрелки/script.js
@@ -0,0 +1,26 @@
+/* https://learn.javascript.ru/task/rewrite-arrow
+
+Перепишите с использованием функции-стрелки
+Замените код Function Expression стрелочной функцией: */
+
+function ask(question, yes, no) {
+	if (confirm(question)) yes()
+	else no()
+}
+
+/* ask(
+	'Вы согласны?',
+	function () {
+		alert('Вы согласились.')
+	},
+	function () {
+		alert('Вы отменили выполнение.')
+	}
+) */
+
+// Решение
+ask(
+	'Вы согласны?',
+	() => alert('Вы согласились.'),
+	() => alert('Вы отменили выполнение.')
+)
